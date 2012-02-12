7c82fb84402e0495b2b5c93dcbb86a3433be3c00
Chris LaRose <cjlarose@gmail.com>
2012-02-11 22:21:38 -0700

    Initial commit

---
+++ inc/wpcom.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WordPress.com-specific functions and definitions
+ *
+ * @package _s
+ * @since _s 1.0
+ */
+
+/**
+ * Set a default theme color array for WP.com.
+ *
+ * @since _s 1.0
+ */
+$themecolors = array(
+	'bg' => '',
+	'border' => '',
+	'text' => '',
+	'link' => '',
+	'url' => '',
+);
